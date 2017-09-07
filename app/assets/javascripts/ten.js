2d3a5821782687aca57fba3639c17f9568c7d166
mnjusto <maronjusto@gmail.com>
2017-09-07 20:58:04 +0800

    additional codes

--- app/assets/javascripts/ten.js
+++ app/assets/javascripts/ten.js
@@ -1,43 +1,34 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
-var ballRadius = 10;
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
-var fastball = 10;
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
-$(document).keydown(function(e){
-	if(e.keyCode == 39) {
-    rightPressed = true;
-  }
-  else if(e.keyCode == 37) {
-    leftPressed = true;
-  }
-})
-
-
-$(document).keyup(function(e){
-	if(e.keyCode == 39) {
-    rightPressed = false;
-  }
-  else if(e.keyCode == 37) {
-    leftPressed = false;
-  }
-})
-
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
 }
 
 function drawBall() {
@@ -47,31 +38,47 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
 }
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
 function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawBall();
-	drawPaddle();
-
-	if(x + dx > (canvas.width - ballRadius) || x + dx < ballRadius) {
-  	dx = -dx;
-	}
-
-	if(y + dy > (canvas.height - ballRadius) || y + dy < ballRadius) {
-    dy = -dy;
-	}	
-
-	if(rightPressed && paddleX < canvas.width-paddleWidth) {
-    paddleX += 7;
-	}
-	else if(leftPressed && paddleX > 0) {
-    paddleX -= 7;
-	}
-
-	x += dx;
-  y += dy;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    }
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+        }
+    }
+    
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+    
+    x += dx;
+    y += dy;
 }
-setInterval(draw, fastball);
+
+setInterval(draw, 10);
 
 // setInterval(function(){fastball -= 1}, 1000);
 
