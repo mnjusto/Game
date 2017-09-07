48f1a383ceb786bb8562fb3c94e26ad6d644e0be
mnjusto <maronjusto@gmail.com>
2017-09-07 20:42:51 +0800

    Initial Commit

---
+++ app/assets/javascripts/ten.js
@@ -0,0 +1,94 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+var fastball = 10;
+
+$(document).keydown(function(e){
+	if(e.keyCode == 39) {
+    rightPressed = true;
+  }
+  else if(e.keyCode == 37) {
+    leftPressed = true;
+  }
+})
+
+
+$(document).keyup(function(e){
+	if(e.keyCode == 39) {
+    rightPressed = false;
+  }
+  else if(e.keyCode == 37) {
+    leftPressed = false;
+  }
+})
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+	drawPaddle();
+
+	if(x + dx > (canvas.width - ballRadius) || x + dx < ballRadius) {
+  	dx = -dx;
+	}
+
+	if(y + dy > (canvas.height - ballRadius) || y + dy < ballRadius) {
+    dy = -dy;
+	}	
+
+	if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
+	}
+	else if(leftPressed && paddleX > 0) {
+    paddleX -= 7;
+	}
+
+	x += dx;
+  y += dy;
+}
+setInterval(draw, fastball);
+
+// setInterval(function(){fastball -= 1}, 1000);
+
+// ctx.beginPath();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle = "#FF0000";
+// ctx.fill();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.arc(240, 160, 20, 0, Math.PI*2, false);
+// ctx.fillStyle = "green";
+// ctx.fill();
+// ctx.closePath();
+
+// ctx.beginPath();
+// ctx.rect(160, 10, 100, 40);
+// ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
+// ctx.stroke();
+// ctx.closePath();
---
+++ test/controllers/ten_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TenControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
